2c3f042e8b2bbd4ad5d74300cbea8036e83a9d13
husky <gpixlexl@gmail.com>
2020-04-14 18:52:25 -0700

    creating the project

---
+++ src/test/java/execution/Base.java
@@ -0,0 +1,32 @@
+package execution;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Base {
+public static WebDriver driver;
+	@Test
+	public void startup() {
+		
+		
+	
+		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\driver\\chromedriver.exe");
+		driver= new ChromeDriver();
+		driver.get("https://www.google.com");
+		
+		
+		
+	
+		
+		
+		
+		
+		
+	}
+	
+	// add screenshots 
+	
+	
+	
+}
